56554c939a80068ce7119caafbcc59293c8dd212
fabriziobagala <fabrizio.bagala@outlook.com>
2025-03-03 16:25:26 +0100

    Remove unused imageToDataUrl method from FaviconCache class

--- src/scripts/favicon-cache.js
+++ src/scripts/favicon-cache.js
@@ -57,22 +57,6 @@ class FaviconCache {
     }
 
     /**
-     * Convert an image to data URL
-     * @param {HTMLImageElement} img - Image element
-     * @returns {string} - Data URL
-     */
-    imageToDataUrl(img) {
-        const canvas = document.createElement('canvas');
-        canvas.width = img.width;
-        canvas.height = img.height;
-
-        const ctx = canvas.getContext('2d');
-        ctx.drawImage(img, 0, 0);
-
-        return canvas.toDataURL('image/png');
-    }
-
-    /**
      * Store favicon in cache
      * @param {string} domain - Domain
      * @param {string} url - URL of favicon
